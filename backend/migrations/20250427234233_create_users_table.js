ea59066d6dcf0da967c81cdede1d4c44dc40a455
brepp22 <brepp@hotmail.com>
2025-08-02 15:38:49 -0700

    rollback and migration updated to support bio

--- backend/migrations/20250427234233_create_users_table.js
+++ backend/migrations/20250427234233_create_users_table.js
@@ -14,6 +14,7 @@ exports.up = function(knex) {
       table.string('password').notNullable(); 
       table.string('role').notNullable();
       table.string('photo');
+      table.text('bio');
       table.timestamp('created_at').defaultTo(knex.fn.now());  
     });
   };
